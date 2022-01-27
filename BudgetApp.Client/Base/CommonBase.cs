d6dc08cdc00c4ffce2995a44ce33978393968be8
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-27 23:57:40 +0800

    Added parent null checker in CommonBase

--- BudgetApp.Client/Base/CommonBase.cs
+++ BudgetApp.Client/Base/CommonBase.cs
@@ -8,5 +8,16 @@ namespace BudgetApp.Client.Base
         public RenderFragment? ChildContent { get; set; }
         [Parameter(CaptureUnmatchedValues = true)]
         public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+        protected virtual void CheckParentIfNull<TParent, TChild>(TParent parent, TChild child)
+        {
+            if (parent == null)
+            {
+                Type parentType = typeof(TParent);
+                Type childType = typeof(TChild);
+                string message = $"{childType} must be inside of {parentType}.";
+                throw new ArgumentNullException(nameof(parent), message);
+            }
+        }
     }
 }
