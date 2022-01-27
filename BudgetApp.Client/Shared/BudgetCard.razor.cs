a38edc2c685350a8c1eec036c11b2c72c0b94f9d
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-28 03:07:20 +0800

    Updated Budget Card.

---
+++ BudgetApp.Client/Shared/BudgetCard.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Shared
+{
+    public partial class BudgetCard : ComponentBase
+    {
+        [Parameter]
+        public string? Name { get; set; }
+        [Parameter]
+        public double Amount { get; set; }
+        [Parameter]
+        public double Max { get; set; }
+
+        internal string? Variant
+        {
+            get
+            {
+                var ratio = Amount / Max;
+                if (ratio < 0.5)
+                {
+                    return "bg-primary";
+                }
+                else if (ratio < 0.75)
+                {
+                    return "bg-warning";
+                }
+                return "bg-danger";
+            }
+        }
+    }
+}
