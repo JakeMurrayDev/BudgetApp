a3076527a508f99b6ac32d50f9fca2008e038705
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-27 22:56:17 +0800

    Added CommonBase

---
+++ BudgetApp.Client/Base/CommonBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Base
+{
+    public abstract class CommonBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+    }
+}
