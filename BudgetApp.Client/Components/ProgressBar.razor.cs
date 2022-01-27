279ab8af99c75b64925a73132119f45503ceabf5
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-28 02:52:17 +0800

    Added ProgressBar Component

---
+++ BudgetApp.Client/Components/ProgressBar.razor.cs
@@ -0,0 +1,22 @@
+﻿using BudgetApp.Client.Base;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Components
+{
+    public partial class ProgressBar : CommonBase
+    {
+        [CascadingParameter]
+        public Progress? Parent { get; set; }
+        [Parameter]
+        [EditorRequired]
+        public double? Min { get; set; }
+        [Parameter]
+        [EditorRequired]
+        public double? Value { get; set; }
+        [Parameter]
+        [EditorRequired]
+        public double? Max { get; set; }
+
+        internal double? Percentage => Value / Max * 100;
+    }
+}
