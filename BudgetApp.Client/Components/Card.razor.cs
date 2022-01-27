6f5e4976c6ea88e2ecac171acfdba63b6b2fe5ac
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-28 01:48:27 +0800

    Added Card Component

---
+++ BudgetApp.Client/Components/Card.razor.cs
@@ -0,0 +1,9 @@
+﻿using BudgetApp.Client.Base;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Components
+{
+    public partial class Card : CommonBase
+    {
+    }
+}
---
+++ BudgetApp.Client/Components/CardTitle.razor.cs
@@ -0,0 +1,17 @@
+﻿using BudgetApp.Client.Base;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Components
+{
+    public partial class CardTitle : CommonBase
+    {
+        [CascadingParameter]
+        public Card? Parent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            CheckParentIfNull(Parent, this);
+        }
+    }
+}
