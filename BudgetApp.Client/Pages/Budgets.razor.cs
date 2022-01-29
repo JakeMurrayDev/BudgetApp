b4382b32297877851ca863a2e71eaa67d4f77473
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-29 19:25:49 +0800

    Updated Budgets Page and AddBudgetModal.

---
+++ BudgetApp.Client/Pages/Budgets.razor.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using BudgetApp.Client.Shared;
+using BudgetApp.Client.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Pages
+{
+    public partial class Budgets : ComponentBase, IDisposable
+    {
+        [Inject]
+        public IBudgetsPageViewModel BudgetsPageViewModel { get; set; }
+
+        private AddBudgetModal _addBudgetModal;
+        private PropertyChangedEventHandler _budgetsPagePropertyChangedEventHandler;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _budgetsPagePropertyChangedEventHandler = (s, e) => StateHasChanged();
+            BudgetsPageViewModel.PropertyChanged += _budgetsPagePropertyChangedEventHandler;
+            await BudgetsPageViewModel.Initialize();
+        }
+
+        public void Dispose()
+        {
+            BudgetsPageViewModel.PropertyChanged -= _budgetsPagePropertyChangedEventHandler;
+        }
+    }
+}
---
+++ BudgetApp.Client/Shared/AddBudgetModal.razor.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using BudgetApp.Client.Components;
+using BudgetApp.Client.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Shared
+{
+    public partial class AddBudgetModal : ComponentBase, IDisposable
+    {
+        [CascadingParameter]
+        public IBudgetsPageViewModel BudgetsPageViewModel { get; set; }
+
+        private Modal _modal;
+        private PropertyChangedEventHandler _eventHandler;
+        private IBudgetViewModel _budget;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            _budget = new BudgetViewModel();
+            _eventHandler = (s, e) => StateHasChanged();
+            _budget.PropertyChanged += _eventHandler;
+        }
+
+        public void Show()
+        {
+            _modal.Show();
+        }
+
+        public void Close()
+        {
+            _modal.Close();
+        }
+
+        private void AddBudget()
+        {
+            _budget.Id = Guid.NewGuid();
+            BudgetsPageViewModel.AddBudget(_budget);
+            _budget = new BudgetViewModel();
+            _modal.Close();
+        }
+
+        public void Dispose()
+        {
+            _budget.PropertyChanged -= _eventHandler;
+        }
+    }
+}
