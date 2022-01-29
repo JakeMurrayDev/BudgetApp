9a54b957437fa1ff8d1abe044825c5b099f604be
Jake Alexander Murray <murray.ja@acenergy.com.ph>
2022-01-29 14:18:06 +0800

    Added button component

---
+++ BudgetApp.Client/Components/Button.razor.cs
@@ -0,0 +1,35 @@
+﻿using BudgetApp.Client.Base;
+using BudgetApp.Client.Enumerations;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetApp.Client.Components
+{
+    public partial class Button : CommonBase
+    {
+        [Parameter]
+        public ButtonVariant Variant { get; set; }
+
+        private string VariantClass
+        {
+            get
+            {
+                return Variant switch
+                {
+                    ButtonVariant.Primary => "btn-primary",
+                    ButtonVariant.Secondary => "btn-secondary",
+                    ButtonVariant.Warning => "btn-warning",
+                    ButtonVariant.Info => "btn-info",
+                    ButtonVariant.Danger => "btn-danger",
+                    ButtonVariant.Light => "btn-light",
+                    ButtonVariant.Dark => "btn-dark",
+                    ButtonVariant.OutlinePrimary => "btn-outline-primary",
+                    ButtonVariant.OutlineSecondary => "btn-outline-secondary",
+                    ButtonVariant.OutlineWarning => "btn-outline-warning",
+                    ButtonVariant.OutlineInfo => "btn-outline-info",
+                    ButtonVariant.OutlineDanger => "btn-outline-danger",
+                    _ => "btn-primary"
+                };
+            }
+        }
+    }
+}
---
+++ BudgetApp.Client/Enumerations/ButtonEnumerations.cs
@@ -0,0 +1,18 @@
+﻿namespace BudgetApp.Client.Enumerations
+{
+    public enum ButtonVariant
+    {
+        Primary,
+        Secondary,
+        Warning,
+        Info,
+        Danger,
+        Light,
+        Dark,
+        OutlinePrimary,
+        OutlineSecondary,
+        OutlineWarning,
+        OutlineInfo,
+        OutlineDanger,
+    }
+}
